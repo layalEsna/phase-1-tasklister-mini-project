959a2a32641db4329e76362a60f9c65bea83fca9
Amene <layla_1380@yahoo.com>
2024-04-17 17:30:48 -0500

    to do list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById('create-task-form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    creatingElements(e.target.description.value)
+    form.reset()
+
+  })
+
+
 });
+function creatingElements(toDo) {
+  console.log(toDo)
+  const li = document.createElement('li')
+  li.textContent = `${toDo} `
+  const ul = document.getElementById('tasks')
+  ul.appendChild(li)
+  const btn = document.createElement('button')
+  btn.textContent = 'x'
+  li.appendChild(btn)
+  btn.addEventListener('click', (e) => {
+    e.target.parentNode.remove()
+    //p.parentNode.remove()
+  })
+}
